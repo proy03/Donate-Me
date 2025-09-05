6a0ccc29efaa704803af1a9e00b6f3c2775316f7
Priyanshu Roy <proy@Roys-MacBook-Air.local>
2025-09-05 12:58:49 +0530

    Ignition deploy modules

--- ignition/modules/deploy.js
+++ ignition/modules/deploy.js
@@ -1,10 +1,8 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("SimpleStorageModule", (m) => {
-  // Deploy SimpleStorage
   const simpleStorage = m.contract("SimpleStorage");
 
-  // Example interaction (optional, Ignition lets you script state changes)
   const setValue = m.call(simpleStorage, "store", [7]);
 
   return { simpleStorage, setValue };
